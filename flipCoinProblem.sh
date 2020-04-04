a2d9f0e5ef5bdd1a73ccc79d068bc2ef9a984a3d
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-04 08:26:03 -1200

    ADDED: doublet to store percentage in dictionary

--- flipCoinProblem.sh
+++ flipCoinProblem.sh
@@ -15,23 +15,39 @@ function flippingCoin()
 	echo "Flip a coin"
 	for ((i=0;i<$1;i++))
 	do
-		if [[ "$(random)" -eq 0 ]]
+		if [[ (("$(random)" -eq 0)) && (("$(random)" -eq 0))]]
 		then
-			((flipCoin["HEADS"]++))
+			((flipCoin["(HEADS HEADS)"]++))
+		elif [[ (("$(random)" -eq 0)) && (("$(random)" -eq 1))]]
+		then
+			((flipCoin["(HEADS TAILS)"]++))
+		elif [[ (("$(random)" -eq 1)) && (("$(random)" -eq 0))]]
+		then
+			((flipCoin["(TAILS HEADS)"]++))
 		else
-			((flipCoin["TAILS"]++))
+			((flipCoin["(TAILS TAILS)"]++))
 		fi
 	done
-	calculatePercentage
+	storePercentageInDictionary
 }
 
-#calculate percentage
-function calculatePercentage()
+#calculate and store percentage in dictionary
+function storePercentageInDictionary()
 {
-	for val in "${!flipCoin[@]}"
-	do
-		echo "${val} percentage is " `awk "BEGIN {print ${flipCoin[$val]}/$counter*100}"`
-	done
+   for val in "${!flipCoin[@]}"
+   do
+      flipCoin[$val]="`awk "BEGIN {print ${flipCoin[$val]}/$counter*100}"`"
+   done
+   printPercentage
+}
+
+#print percentage
+function printPercentage
+{
+   for val in "${!flipCoin[@]}"
+   do
+      echo "${val} percentage is ${flipCoin[$val]}"
+   done
 }
 
 flippingCoin $counter

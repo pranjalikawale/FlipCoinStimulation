a76dd7b334ee5442f0237b98bd9037bcbcab1f6e
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-04 08:11:59 -1200

    ADDED: Singlet flip coin to calculate percentage

--- flipCoinProblem.sh
+++ flipCoinProblem.sh
@@ -1,5 +1,8 @@
 #!/bin/bash -x
 
+read -p "Number of time you flip coin " counter
+declare -A flipCoin
+
 #find random number
 function random()
 {
@@ -7,15 +10,28 @@ function random()
 }
 
 #find Heads or Tails in flip coin
-function flipCoin()
+function flippingCoin()
 {
-	if [[ "$(random)" -eq 0 ]]
-	then
-		echo "HEADS"
-	else
-		echo "TAILS"
-	fi
+	echo "Flip a coin"
+	for ((i=0;i<$1;i++))
+	do
+		if [[ "$(random)" -eq 0 ]]
+		then
+			((flipCoin["HEADS"]++))
+		else
+			((flipCoin["TAILS"]++))
+		fi
+	done
+	calculatePercentage
 }
 
-echo "Flip a coin"
-flipCoin
+#calculate percentage
+function calculatePercentage()
+{
+	for val in "${!flipCoin[@]}"
+	do
+		echo "${val} percentage is " `awk "BEGIN {print ${flipCoin[$val]}/$counter*100}"`
+	done
+}
+
+flippingCoin $counter

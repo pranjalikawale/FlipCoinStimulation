8119aab75fea113180760d62efaa9f1459ccca10
pranjalikawale <pranjalikawale1578@gmail.com>
2020-04-04 08:58:50 -1200

    ADDED: Singlet doublet triplet combination of coin

--- flipCoinProblem.sh
+++ flipCoinProblem.sh
@@ -2,6 +2,7 @@
 
 read -p "Number of time you flip coin " counter
 declare -A flipCoin
+win=0
 
 #find random number
 function random()
@@ -9,10 +10,43 @@ function random()
 	echo $((RANDOM%2))
 }
 
-#find Heads or Tails in flip coin
-function flippingCoin()
+#find Singlet combination in coin
+function singlet()
 {
-   echo "Flip a coin"
+	for ((i=0;i<$1;i++))
+	do
+		if [[ "$(random)" -eq 0 ]]
+		then
+			((flipCoin["HEADS"]++))
+		else
+			((flipCoin["TAILS"]++))
+		fi
+	done
+}
+
+#find doublet combination in coin
+function doublet()
+{
+   for ((i=0;i<$1;i++))
+   do
+      if [[ (("$(random)" -eq 0)) && (("$(random)" -eq 0))]]
+      then
+         ((flipCoin["(HEADS HEADS)"]++))
+      elif [[ (("$(random)" -eq 0)) && (("$(random)" -eq 1))]]
+      then
+         ((flipCoin["(HEADS TAILS)"]++))
+      elif [[ (("$(random)" -eq 1)) && (("$(random)" -eq 0))]]
+      then
+         ((flipCoin["(TAILS HEADS)"]++))
+      else
+         ((flipCoin["(TAILS TAILS)"]++))
+      fi
+   done
+}
+
+#find triplet combination in coin
+function triplet()
+{
    for ((i=0;i<$1;i++))
    do
       if [[ (("$(random)" -eq 0)) && (("$(random)" -eq 0)) && (("$(random)" -eq 0))]]
@@ -40,26 +74,62 @@ function flippingCoin()
          ((flipCoin["(TAILS TAILS TAILS)"]++))
       fi
    done
-   storePercentageInDictionary
 }
 
-#calculate and store percentage in dictionary
-function storePercentageInDictionary()
+#calculate percentage & store in dictionary
+function calculatePercentage()
 {
    for val in "${!flipCoin[@]}"
    do
-      flipCoin[$val]="`awk "BEGIN {print ${flipCoin[$val]}/$counter*100}"`"
+      flipCoin[$val]=$((flipCoin[$val]*100/counter))
    done
-   printPercentage
 }
 
-#print percentage
-function printPercentage
+#create array of dictionary
+function createArray()
+{
+   count=0
+   for val in "${flipCoin[@]}"
+   do
+      resultArray[((count++))]=$val
+   done
+   sortDescending "${resultArray[@]}"
+}
+
+#sort the array to get highest percentage
+function sortDescending()
+{
+   array=("${@}")
+   for ((i=0;i<$((${#array[@]}-1));i++))
+   do
+      for ((j=$((i+1));j<${#array[@]};j++))
+      do
+         if [[ ${array[$i]} -le ${array[$j]} ]]
+         then
+            temp=${array[$i]}
+            array[$i]=${array[$j]}
+            array[$j]=$temp
+         fi
+      done
+   done
+   win=${array[0]}
+}
+
+#print the winnig Combination
+function printWinningCombination
 {
    for val in "${!flipCoin[@]}"
    do
-      echo "${val} percentage is ${flipCoin[$val]}"
+      if [[ (($win -eq ${flipCoin[$val]})) ]]
+      then
+         echo "${val} is winning combination ${flipCoin[$val]}"
+      fi
    done
 }
 
-flippingCoin $counter
+singlet $counter
+doublet $counter
+triplet $counter
+calculatePercentage
+createArray
+printWinningCombination
